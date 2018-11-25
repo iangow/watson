cfc289fab426f96ac725d5e4db7a3d9b64149762
Ian Gow <iandgow@gmail.com>
2018-11-25 14:39:06 +1100

    More changes due to change in schema and server.
    - Relates to https://github.com/vcherep/personality/issues/2.

--- create_watson_output.py
+++ create_watson_output.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-conn_string = "postgresql://aaz.chicagobooth.edu:5432/postgres"
+conn_string = "postgresql://aaz2.chicagobooth.edu:5432/postgres"
 
 import pandas as pd
 import json 
@@ -12,8 +12,8 @@ db_engine = create_engine(conn_string)
 
 sql = """
     SELECT company_id, executive_id, file_name, profile
-    FROM personality.watson_output_raw
-    INNER JOIN personality.watson_input
+    FROM big5.watson_output_raw
+    INNER JOIN big5.watson_input
     USING (hash)
     """
     
@@ -22,5 +22,5 @@ df = pd.read_sql(sql, db_engine)
 df['scores'] = df['profile'].map(lambda x: extract_scores(x))
 df_mod = expand_json(df.drop(['profile'], axis=1), 'scores')
 
-df_mod.to_sql("watson_output_alt", db_engine, "personality", 
+df_mod.to_sql("watson_output_alt", db_engine, "big5", 
               if_exists = 'replace', index = False)
